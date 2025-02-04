0605eef9a9c58baa1e8ff6abda249f4d261e1c37
Arinal Hamara <arinal.kantor@gmail.com>
2025-02-04 10:03:20 +0700

    Home Fire Detection and Water Pump System via NodeMCU ESP8266
    This project aims to develop an automatic fire detection and suppression system to improve home safety using Internet of Things (IoT) technology. The system is designed to detect fires, automatically activate a water pump, and send notifications to homeowners via WhatsApp.
    
    Main Features:
    ▪ Fire Detection – Uses temperature and smoke sensors to quickly detect potential fires.
    
    ▪ Automatic Fire Suppression – Activates a water pump automatically to extinguish the fire when detected.
    
    ▪ Real-Time Notifications – Sends alerts via WhatsApp to homeowners and emergency contacts, allowing for a quick response.
    
    ▪ Remote Monitoring – Allows homeowners to check the system’s status online.
    
    Technologies Used:
    ▪ NodeMCU ESP8266 – The main microcontroller that connects the system to the internet.
    
    ▪ Temperature and Smoke Sensors – Detect potential fire hazards.
    
    ▪ Water Pump – Functions as an automatic fire suppression mechanism.
    
    ▪ WhatsApp API – Sends real-time notifications to users.
    
    This project not only provides an innovative home security solution but also offers valuable experience in IoT-based system development and automation. I hope this project contributes to improving safety in residential areas.

---
+++ Home Fire Detection and Water Pump System via NodeMCU ESP8266.cpp
@@ -0,0 +1,167 @@
+//Include the library
+#include <MQUnifiedsensor.h>
+#include <ESP8266WiFi.h>
+#include <ThingESP.h>
+
+//Definitions
+#define placa "Arduino UNO"
+#define Voltage_Resolution 5
+#define pin A0 
+#define type "MQ-135" //MQ135
+#define ADC_Bit_Resolution 10 
+#define RatioMQ135CleanAir 3.6 
+#define FlameSensor1 14
+#define FlameSensor2 12
+#define Buzz 0
+#define Buzz2 15
+#define Pompa1 16
+#define Pompa2 2
+#define Fan 3
+float Tambah, Tambah2;
+
+ThingESP8266 thing("arinalhamara", "MonitoringRuang", "fireandair");
+
+
+//Declare Sensor
+MQUnifiedsensor MQ135(placa, Voltage_Resolution, ADC_Bit_Resolution, pin, type);
+char ssid[] = "SERENITY_plus";
+char pass[] = "babykenya";
+
+
+
+void setup() {
+  //Init the serial port communication - to debug the library
+  Serial.begin(115200); //Init serial port
+  thing.SetWiFi(ssid, pass);
+  thing.initDevice();
+  
+  pinMode(FlameSensor1, INPUT);
+  pinMode(FlameSensor2, INPUT);
+  pinMode(Buzz, OUTPUT);
+  pinMode(Buzz2, OUTPUT); 
+  pinMode(Pompa1, OUTPUT);
+  pinMode(Pompa2, OUTPUT);
+  pinMode(Fan, OUTPUT);
+  digitalWrite(Buzz, LOW);
+  digitalWrite(Buzz2, LOW);
+  digitalWrite(Pompa1, LOW);
+  digitalWrite(Pompa2, LOW);
+  digitalWrite(Fan, LOW);
+  Tambah = 0;
+  Tambah2 = 0;
+  
+  
+  //Set math model to calculate the PPM concentration and the value of constants
+  MQ135.setRegressionMethod(1); //_PPM =  a*ratio^b
+  MQ135.init(); 
+  Serial.print("Calibrating please wait.");
+  float calcR0 = 0;
+  for(int i = 1; i<=10; i ++)
+  {
+    MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+    calcR0 += MQ135.calibrate(RatioMQ135CleanAir);
+    Serial.print(".");
+  }
+  MQ135.setR0(calcR0/10);
+  Serial.println("  done!.");
+  
+  if(isinf(calcR0)) {Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply"); while(1);}
+  if(calcR0 == 0){Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply"); while(1);}
+  /**********  MQ CAlibration ***************/ 
+}
+
+String HandleResponse(String query)
+{
+   MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+
+  MQ135.setA(80.897); MQ135.setB(-2.431); // Configurate the ecuation values to get CO concentration
+  float LPG= MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(110.47); MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+  float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  Tambah = (LPG+150);
+  Tambah2 = (CO2+400);
+
+  if (query == "Kualitas"){
+    return "Gas: " + String(Tambah);
+    
+  }
+  else if(query == "CO2"){
+    return "CO2: " + String(Tambah2);
+  }
+  else return "SALAH ENTRI";
+  
+}
+void loop() {
+  int FireSensor1 = digitalRead(FlameSensor1);
+  int FireSensor2 = digitalRead(FlameSensor2);
+
+  thing.Handle();
+  
+  MQ135.update(); // Update data, the arduino will be read the voltage on the analog pin
+
+  MQ135.setA(80.897); MQ135.setB(-2.431); // Configurate the ecuation values to get CO concentration
+  float LPG= MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  MQ135.setA(110.47); MQ135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration
+  float CO2 = MQ135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+
+  Tambah = (LPG+150);
+  Tambah2 = (CO2+400);
+  Serial.print("Kadar LPG : "); 
+  Serial.print(Tambah);
+  Serial.println(" ppm");
+   
+  Serial.print("Kadar CO2 : "); 
+  Serial.print(Tambah2); 
+  Serial.println(" ppm");
+
+  Serial.println("----------------------");
+  
+  Serial.print("Nilai Flame Sensor 1 : ");
+  Serial.println(FireSensor1);
+  Serial.print("Nilai Flame Sensor 2 : ");
+  Serial.println(FireSensor2);
+
+  Serial.println("----------------------");
+  
+  /*
+    Exponential regression:
+  GAS      | a      | b
+  CO       | 605.18 | -3.937  
+  Alcohol  | 77.255 | -3.18 
+  CO2      | 110.47 | -2.862
+  Tolueno  | 44.947 | -3.445
+  NH4      | 102.2  | -2.473
+  Acetona  | 34.668 | -3.369
+  */
+  if(FireSensor1 == 0)
+  {
+    digitalWrite(Pompa1, HIGH);
+    digitalWrite(Buzz, HIGH);
+  }
+  if(FireSensor2 == 0)
+  {
+    digitalWrite(Pompa2, HIGH);
+    digitalWrite(Buzz2, HIGH);
+  }
+  if(FireSensor2 == 1){
+    digitalWrite(Pompa2, LOW);
+    digitalWrite(Buzz2, LOW);
+  }
+  if(FireSensor1 == 1){
+    digitalWrite(Pompa1, LOW);
+    digitalWrite(Buzz, LOW);
+  }
+  if((Tambah2)>=600 || (Tambah)>= 400)
+  {
+    digitalWrite(Fan, HIGH);
+  }
+  if((Tambah2)<500 || (Tambah)< 300)
+  {
+    digitalWrite(Fan, LOW);
+  }
+  
+  delay(1000); //Sampling frequency
+}
